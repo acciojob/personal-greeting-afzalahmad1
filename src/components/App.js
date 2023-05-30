2af19f42e2e98869ec30d2ca7180e20529c8bf09
Afzal Ahmad <afzalm117@gmail.com>
2023-05-31 00:34:30 +0530

    com

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  const [name,setName] = useState("");
   return (
     <div>
         {/* Do not remove the main div */}
+        <label htmlFor="name">Enter Your Name</label>
+        <br/>
+        <input type="text"
+        id="name"
+        value={name}
+        onChange={(e)=> setName(e.target.value)}
+          />
+
+          {name  && <p>Hello {name}</p>}
     </div>
   )
 }
